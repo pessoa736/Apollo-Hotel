bcef247a1568f1d9918ca5bcfc82be8fda4fbc61
Thaynix <thaygoncalvesjg@gmail.com>
2026-04-11 13:46:06 -0300

    feat: adiciona componente modal nos templates

---
+++ static/js/main.js
@@ -0,0 +1,21 @@
+var modal = document.getElementById("modal");
+var btn = document.getElementById("openModal");
+var span = document.getElementsByClassName("close")[0];
+
+// Abre o modal :D
+btn.onclick = function(e) {
+  e.preventDefault(); // O link nao redireciona
+  modal.style.display = "flex";
+}
+
+// Fechar no X
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// Fechar clicando fora
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "flex";
+  }
+}
